4260b3d3bb220af62cba88fa7e2dbade549b5513
BaoBao79 <101114804+BaoBao79@users.noreply.github.com>
2022-03-08 12:31:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const CONFIG = {
     titleWeb: "Bảo Bảo😘",
-    introTitle: 'HI Quỳnh Anh 😘',
+    introTitle: 'HI Thùy Dương 😘',
     introDesc: `Trái đất vốn lạ thường
     Mà sao cậu cứ đi nhầm đường
     Lạc vào tim tớ lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: '🥰Ngồi chờ duyên phận ý trời',
+    title: Ngồi chờ duyên phận ý trời',
     desc: 'Phải chăng Chi bằng chủ động ngỏ lời thương nhau! ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
@@ -14,7 +14,7 @@ const CONFIG = {
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'chúc cậu mùng 8/3 vui vẻ ngày càng xinh đẹp trong mắt tớ nhaaaaa, iu cậu nhìu ạ <33333.',
+    messDesc: 'chúc cậu mùng 8/3 vui vẻ ngày càng xinh đẹp trong mắt tớ nhaaaaa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: https://www.facebook.com/dii.hanthien.90857' 
+    messLink: https://www.facebook.com/dthdg12' 
 }
